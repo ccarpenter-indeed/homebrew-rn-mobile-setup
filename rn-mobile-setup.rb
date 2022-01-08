5524f9db61f3e33eee91aee0250b63d081c29b77
Caleb Carpenter <ccarpenter@indeed.com>
2022-01-08 16:42:59 -0600

    Updated to v0.0.4

--- rn-mobile-setup.rb
+++ rn-mobile-setup.rb
@@ -1,8 +1,8 @@
 class RnMobileSetup < Formula
   desc "Bootstrap the React Native mobile development environment."
   homepage "https://github.com/ccarpenter-indeed/rn-mobile-setup"
-  url "https://github.com/ccarpenter-indeed/rn-mobile-setup/archive/refs/tags/v0.0.3.tar.gz"
-  sha256 "e46130a0dce5e486fe0817b208a5cd6447b86dcd3b32e57e339e45f81f05fdc6"
+  url "https://github.com/ccarpenter-indeed/rn-mobile-setup/archive/refs/tags/v0.0.4.tar.gz"
+  sha256 "5ec82a9e10c0c02877c439f970e93c34eb7bd929034e81f65fccdb6b9828e186"
   license "MIT"
 
   def install
